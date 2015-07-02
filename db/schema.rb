d17bd9f7e927a0e5a1dab502a305ab0f58e735e3
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-07-02 12:33:05 +0000

    add column age

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630192519) do
+ActiveRecord::Schema.define(version: 20150702093019) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
