172ac6b5637852deed897f18b7ecff928a251f0f
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-07-03 14:20:54 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703140629) do
+ActiveRecord::Schema.define(version: 20150703141646) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
